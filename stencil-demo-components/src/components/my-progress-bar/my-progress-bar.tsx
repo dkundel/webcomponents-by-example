a6a4746a59d8feff2d0a059e53c438afac798a03
Dominik Kundel <dominik.kundel@gmail.com>
2018-02-08 15:28:52 +0100

    :sparkles: Add first stencil demo component

---
+++ stencil-demo-components/src/components/my-progress-bar/my-progress-bar.tsx
@@ -0,0 +1,23 @@
+import { Component, Prop, Element } from '@stencil/core';
+
+@Component({
+  tag: 'my-progress-bar',
+  styleUrl: 'my-progress-bar.scss'
+})
+export class MyProgressBar {
+  @Prop() value: number;
+  @Prop() max: number = 100;
+  @Prop() showValue: boolean = false;
+
+  @Element() el: HTMLElement;
+
+  render() {
+    this.el.style.setProperty('--current-value', this.value.toString());
+    this.el.style.setProperty('--max-value', this.max.toString());
+
+    return [
+      <div class="progress-bar">{this.showValue ? this.value : ' '}</div>,
+      <div class="progress-bar-remainder" />
+    ];
+  }
+}
---
+++ stencil-demo-components/stencil.config.js
@@ -0,0 +1,6 @@
+exports.config = {
+  namespace: 'demo-components',
+  generateDistribution: true,
+  generateWWW: false,
+  bundles: [{ components: ['my-progress-bar'] }]
+};
